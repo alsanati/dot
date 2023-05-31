712d7177ef6e70762004234e0a55eb82a6102259
Atilla Alsan <111384214+alsanati@users.noreply.github.com>
2023-05-31 18:50:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,90 @@
 window.onload = function() {
-    var canvas = document.getElementById('canvas');
-    var ctx = canvas.getContext('2d');
-    var colorPicker = document.getElementById('color-picker');
-    var gridSize = 10; // size of one grid cell
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');
+  var colorPicker = document.getElementById('color-picker');
+  var gridSize = 10;
 
-    canvas.width = 5000;
-    canvas.height = 5000;
-  
-    canvas.addEventListener('click', function(e) {
-      var rect = canvas.getBoundingClientRect();
-      var x = e.clientX - rect.left;
-      var y = e.clientY - rect.top;
-  
-      // Round click coordinates to nearest grid cell
-      var gridX = Math.floor(x / gridSize) * gridSize;
-      var gridY = Math.floor(y / gridSize) * gridSize;
-  
-      // update the color to be used for drawing
-      ctx.fillStyle = colorPicker.value;
-  
-      drawPixel(gridX, gridY);
+  var zoom = 1;
+  var panX = 0;
+  var panY = 0;
+
+  // Set the canvas size
+  canvas.width = 5000;
+  canvas.height = 5000;
+
+  // Create a new Hammer instance for the canvas
+  var hammer = new Hammer.Manager(canvas, {
+    recognizers: [
+      [Hammer.Pinch, {}],
+      [Hammer.Pan, { direction: Hammer.DIRECTION_ALL }],
+    ],
+    touchAction: 'none', // Important to let the native touch events go through
+  });
+
+  function drawPixel(x, y, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, gridSize, gridSize);
+  }
+
+  // Load pixel data from localStorage
+  var pixelDataString = localStorage.getItem('pixelData');
+  if (pixelDataString) {
+    var pixelData = JSON.parse(pixelDataString);
+    pixelData.forEach(pixel => {
+      drawPixel(pixel.x, pixel.y, pixel.color);
     });
-  
-    function drawPixel(x, y) {
-      ctx.fillRect(x, y, gridSize, gridSize);
-    }
-  };
-  
+  } else {
+    pixelData = [];
+  }
+
+  function update() {
+    ctx.setTransform(1, 0, 0, 1, 0, 0); // reset the transform
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.setTransform(zoom, 0, 0, zoom, panX, panY);
+    // redraw stored pixels
+    pixelData.forEach(pixel => {
+      drawPixel(pixel.x, pixel.y, pixel.color);
+    });
+  }
+
+  // Handle pinch
+  hammer.on('pinch', function(e) {
+    zoom = e.scale;
+    update();
+  });
+
+  // Handle pan
+  hammer.on('pan', function(e) {
+    panX = e.deltaX;
+    panY = e.deltaY;
+    update();
+  });
+
+  // Handle drawing
+  canvas.addEventListener('touchstart', function(e) {
+    e.preventDefault();
+
+    // Adjust click coordinates for zoom and pan
+    var rect = canvas.getBoundingClientRect();
+    var x = (e.touches[0].clientX - rect.left - panX) / zoom;
+    var y = (e.touches[0].clientY - rect.top - panY) / zoom;
+
+    var gridX = Math.floor(x / gridSize) * gridSize;
+    var gridY = Math.floor(y / gridSize) * gridSize;
+
+    // Draw pixel on canvas
+    drawPixel(gridX, gridY, colorPicker.value);
+
+    // Store pixel in array
+    pixelData.push({
+      x: gridX,
+      y: gridY,
+      color: colorPicker.value
+    });
+
+    // Save pixel data in localStorage
+    localStorage.setItem('pixelData', JSON.stringify(pixelData));
+  });
+
+  update();
+};
