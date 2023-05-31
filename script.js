40f26af6bd42e202e693e6ce7a2593500af3a969
Atilla Alsan <alsan.atilla@icloud.com>
2023-05-31 17:58:59 +0200

    fixed typos

--- script.js
+++ script.js
@@ -2,68 +2,25 @@ window.onload = function() {
     var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
     var colorPicker = document.getElementById('color-picker');
-    var gridSize = 10;
+    var gridSize = 10; // size of one grid cell
   
-    var zoom = 1;
-    var panX = 0;
-    var panY = 0;
-  
-    // Set the canvas size
-    canvas.width = 5000;
-    canvas.height = 5000;
-  
-    // Create a new Hammer instance for the canvas
-    var hammer = new Hammer.Manager(canvas);
-  
-    // Add pinch and pan recognizers
-    hammer.add(new Hammer.Pinch());
-    hammer.add(new Hammer.Pan());
-  
-    // Update the display whenever the canvas is zoomed or panned
-    function update() {
-      ctx.setTransform(1, 0, 0, 1, 0, 0); // reset the transform
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.setTransform(zoom, 0, 0, zoom, panX, panY);
-      // fetch and draw pixels here...
-    }
-  
-    // Handle pinch
-    hammer.on('pinch', function(e) {
-      zoom = e.scale;
-      update();
-    });
-  
-    // Handle pan
-    hammer.on('pan', function(e) {
-      panX = e.deltaX;
-      panY = e.deltaY;
-      update();
-    });
-  
-    // Handle drawing
-    canvas.addEventListener('touchstart', function(e) {
-      e.preventDefault();
-  
-      // Adjust click coordinates for zoom and pan
+    canvas.addEventListener('click', function(e) {
       var rect = canvas.getBoundingClientRect();
-      var x = (e.touches[0].clientX - rect.left - panX) / zoom;
-      var y = (e.touches[0].clientY - rect.top - panY) / zoom;
+      var x = e.clientX - rect.left;
+      var y = e.clientY - rect.top;
   
+      // Round click coordinates to nearest grid cell
       var gridX = Math.floor(x / gridSize) * gridSize;
       var gridY = Math.floor(y / gridSize) * gridSize;
   
+      // update the color to be used for drawing
       ctx.fillStyle = colorPicker.value;
   
-      // Draw pixel on canvas
       drawPixel(gridX, gridY);
-  
-      // Send pixel to the server...
     });
   
     function drawPixel(x, y) {
       ctx.fillRect(x, y, gridSize, gridSize);
     }
-  
-    update();
   };
   
